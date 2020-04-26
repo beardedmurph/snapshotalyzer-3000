680505c37bca3ae41bba6fde41ce2bcab393a6bf
beardedmurph <darrenkmmurphy@gmail.com>
2020-04-26 15:49:42 +0100

    added a setup file for installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Snapshotalyzer-3000',
+    version='0.1',
+    author="Darren Murphy",
+    description="Snapshotalyzer-3000 is a tool to manage AWS EC2 snapshots",
+    license="GPLv3+",
+    packages=['shotty'],
+    url="https://github.com/beardedmurph/snapshotalyzer-3000",
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        shotty=shotty.shotty:cli
+    '''
+)
